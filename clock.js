8f3fa27bb5cae5f74469f58bb0243e91f6702a60
AVG4 <96074521+AVG4@users.noreply.github.com>
2023-11-04 14:42:21 +0000

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,14 @@
+// document.getElementById('addTimeButton').addEventListener('click', function () {
+
+//     var ukTime = new Date().toLocaleTimeString('en-GB', { timeZone: 'Europe/London' });
+//     var brazilTime = new Date().toLocaleTimeString('en-GB', { timeZone: 'America/Sao_Paulo' });
+
+//     var newUkTimeItem = document.createElement('li');
+//     newUkTimeItem.textContent = 'UK Time: ' + ukTime;
+
+//     var newBrazilTimeItem = document.createElement('li');
+//     newBrazilTimeItem.textContent = 'Brazil Time: ' + brazilTime;
+
+//     document.getElementById('timeList').appendChild(newUkTimeItem);
+//     document.getElementById('timeList').appendChild(newBrazilTimeItem);
+// })
---
+++ script2.js
@@ -0,0 +1,47 @@
+window.addEventListener("load", ()=>{
+    const form = document.querySelector("#search-form")
+    form.addEventListener("submit", getUserDetails)
+
+})
+
+function getUserDetails(event)
+{
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    const data = Object.fromEntries(formData.entries())
+
+    if(data.username == "" || !data.username)
+    {
+        alert("Please enter a valid username")
+        return
+    }
+
+    fetch(`https://api.github.com/users/${data.username}`)
+    .then(response => response.json())
+    .then(apiData => {
+        console.log("entered this section")
+
+        if(apiData.message && apiData.message == "Not Found")
+        {
+            alert("Enter a valid username because this one was not found")
+            return
+        }
+
+        const image = document.querySelector("#image")
+        const h3 = document.querySelector("#name")
+
+        const bioSpan = document.querySelector("#bio")
+        const resposSpan = document.querySelector("#public_repos")
+
+        image.setAttribute("src", apiData.avatar_url)
+
+        h3.innerHTML = apiData.name
+        bioSpan.innerHTML = apiData.bio
+        resposSpan.innerHTML = apiData.public_repos
+
+        console.log("data", apiData)
+    })
+
+
+}
